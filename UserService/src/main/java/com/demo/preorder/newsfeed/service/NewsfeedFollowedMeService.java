38145da5ea173e7769b9d0fa0ec7968fe97d0cda
LeeJaeEun0 <wodmschlrh12@naver.com>
2024-02-04 20:49:30 +0900

    feat - 유저 모듈에서 필요한 로직만 사용

--- UserService/src/main/java/com/demo/preorder/newsfeed/service/NewsfeedFollowedMeService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.preorder.newsfeed.service;
-
-import com.demo.preorder.newsfeed.entity.NewsfeedFollowedMe;
-
-import java.util.List;
-
-public interface NewsfeedFollowedMeService {
-    List<NewsfeedFollowedMe> newsfeedFollowedMe(Long userId);
-}
