ac9b6e36aaecc0cce26a36d1499ce48c5ecf081c
LeeJaeEun0 <wodmschlrh12@naver.com>
2024-02-28 17:26:20 +0900

    feat - ActivityService api 수정

--- ActivityService/src/main/java/com/demo/preorder/post/controller/GreatPostController.java
+++ ActivityService/src/main/java/com/demo/preorder/post/controller/GreatPostController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/greatPosts")
+@RequestMapping("/api/great-posts")
 @RequiredArgsConstructor
 public class GreatPostController {
 
